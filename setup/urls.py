7548a9bc24dc675966e16c0e47dc84ed9ab78d55
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-03-19 16:16:38 -0300

    Implement Django REST framework viewsets for Estudante and Curso models with serializers and URL routing

--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,16 @@
-
 from django.contrib import admin
-from django.urls import path
-from escola.views import estudantes
-
+from django.urls import path, include
+from escola.views import EstudanteViewSet, CursoViewSet
+from rest_framework import routers
 
 # URL Configuration
+router = routers.DefaultRouter()
+router.register('estudantes', EstudanteViewSet, basename="Estudantes")
+router.register('cursos', CursoViewSet, basename="Cursos")
 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('estudantes/', estudantes, name='estudantes')
+
 ]
