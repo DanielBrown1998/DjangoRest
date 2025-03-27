bb21e38412b79708cc2b99fe7e7e9b2202989707
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-03-27 18:37:46 -0300

    Add list views for Matricula by Estudante and Curso, update serializers and views

--- escola/views.py
+++ escola/views.py
@@ -1,13 +1,15 @@
 from escola.models import Estudante, Curso, Matricula
-from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer
-from rest_framework import viewsets
+from escola.serializers import EstudanteSerializer, CursoSerializer, \
+    MatriculaSerializer, ListMatriculaCursoSerializer, ListMatriculaEstudanteSerializer
+from rest_framework import viewsets, generics
 
+1
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
 
 
-class CursoViewSet(viewsets.ModelViewSet):  
+class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     # queryset = Curso.objects.filter(ativo=True)
     serializer_class = CursoSerializer
@@ -16,3 +18,19 @@ class CursoViewSet(viewsets.ModelViewSet):
 class MatriculaViewSet(viewsets.ModelViewSet):
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+
+
+class ListMatriculaEstudante(generics.ListAPIView):
+
+    serializer_class = ListMatriculaEstudanteSerializer
+
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs["pk"])
+        return queryset
+    
+class ListMatriculasCurso(generics.ListAPIView):
+    serializer_class = ListMatriculaCursoSerializer
+
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs["pk"])
+        return queryset
--- setup/urls.py
+++ setup/urls.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListMatriculaEstudante, ListMatriculasCurso
 from rest_framework import routers
 
 # URL Configuration
+
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet, basename="Estudantes")
 router.register('cursos', CursoViewSet, basename="Cursos")
-router.register("Matricula", MatriculaViewSet, basename="Matricula")
-
+router.register("matricula", MatriculaViewSet, basename="Matricula")
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path("estudantes/<int:pk>/matricula/", ListMatriculaEstudante.as_view()),
+    path("cursos/<int:pk>/matricula/", ListMatriculasCurso.as_view()),
 
 ]
